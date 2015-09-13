ac06a0acfb1aa2532a582c6050eaa8c3c6c409a5
Tom Miller <millertp@gmail.com>
2015-09-13 21:31:07 +1000

    Add GUI

--- app.js
+++ app.js
@@ -1,47 +1,139 @@
-var app = require('express')();
-var http = require('http');
+var express = require('express');
+var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io').listen(server);
+
+var mongoose = require('mongoose');
 var request = require('request');
+var path = require('path');
+var bodyParser = require('body-parser');
 
-app.set('port', process.env.PORT || 3000);
+mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://127.0.0.1:27017/apijit');
 
-app.route('/').all(function(req, res) {
-  
-  // STEP 1
-  var CONSUMER_KEY = 'QbcgktohsFb3vepBAyWVwz0cAtmcbm0u';
-  var CONSUMER_SECRET = 'zEZZASrwtAPLcPP6';
-  var url = 'https://api.telstra.com/v1/oauth/token?client_id=' + CONSUMER_KEY + '&client_secret=' + CONSUMER_SECRET + '&grant_type=client_credentials&scope=SMS';
-
-  request({
-      url: url,
-      method: 'GET'
-    }, function(e, r, body) {
-      
-      // STEP 2
-      var auth_data = JSON.parse(body);
-
-      var url = 'https://api.telstra.com/v1/sms/messages';
-      var token = auth_data.access_token;
-      var recipient_number = '0413041313';
-      var message = 'Hi Tom';
-
-      request({
-          headers: {
-            'Content-Type': 'application/json',
-            'Authorization': 'Bearer ' + token
-          },
-          url: url,
-          json: {
-            'to': recipient_number,
-            'body': message
-          },
-          method: 'POST'
-        }, function (e, r, body) {
-          console.log(body);
-          res.send('Done!');
-        });
-    });
+var SMSSchema = mongoose.Schema({
+  messageId: { type: String, index: true },
+  from: String,
+  to: String,
+  timestamp: String,
+  content: String,
+  replies: [{
+    from: String,
+    timestamp: String,
+    content: String
+  }]
 });
 
-app.listen(app.get('port'), function() {
+var SMS = mongoose.model('SMS', SMSSchema);
+
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(bodyParser.urlencoded( { extended: true } ));
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, '/public')));
+
+app.route('/')
+  
+  .get(function(req, res) {
+    res.sendfile('views/index.html');    
+  })
+
+  .post(function(req, res) {
+
+    var from_name = req.body.from_name;
+    var to_number = req.body.to_number;
+    var to_content = req.body.to_content;
+    
+    var CONSUMER_KEY = 'QbcgktohsFb3vepBAyWVwz0cAtmcbm0u';
+    var CONSUMER_SECRET = 'zEZZASrwtAPLcPP6';
+    var url = 'https://api.telstra.com/v1/oauth/token?client_id=' + CONSUMER_KEY + '&client_secret=' + CONSUMER_SECRET + '&grant_type=client_credentials&scope=SMS';
+
+    request({
+        url: url,
+        method: 'GET'
+      }, function (e, r, body) {
+        var token = JSON.parse(body).access_token;
+        
+        request({
+            headers: {
+              'Content-Type': 'application/json',
+              'Authorization': 'Bearer ' + token
+            },
+            url: 'https://api.telstra.com/v1/sms/messages',
+            json: {
+              'to': to_number,
+              'body': to_content + ' - ' + from_name + ' via APIjit.xyz'
+            },
+            method: 'POST'
+          }, function (e, r, body) {
+            var messageId = body.messageId;
+            
+            request({
+              headers: {
+                'Authorization': 'Bearer ' + token
+              },
+              url: 'https://api.telstra.com/v1/sms/messages/' + messageId,
+              method: 'GET'
+            }, function (e, r, body) {
+              var sentTimestamp = JSON.parse(body).sentTimestamp;
+
+              var sms = new SMS({
+                'messageId': messageId,
+                'from': from_name,
+                'to': to_number,
+                'timestamp': sentTimestamp,
+                'content': to_content
+              });
+
+              sms.save();
+
+              res.sendfile('views/index.html');
+            });
+          });
+      });
+  });
+
+app.route('/smsreply')
+  .post(function(req, res) {
+
+    var messageId = req.body.messageId;
+    var from = req.body.from;
+    var timestamp = req.body.acknowledgedTimestamp;
+    var content = req.body.content;
+
+    SMS.findOneAndUpdate(
+      { 'messageId': messageId },
+      { 
+        $push: {
+          'replies': {
+            'from': from,
+            'timestamp': timestamp,
+            'content': content
+          }
+        }
+      },
+      { 'new': true },
+      function(err, sms) {
+        io.emit('new SMS', {
+          'messageId': messageId,
+          'from': from,
+          'to': sms.from,
+          'timestamp': timestamp,
+          'content': content 
+        });
+        res.end();
+      }
+    );
+  });
+
+io.on('connection', function(socket) {
+  SMS.find({}, function(err, smss) {
+    smss.forEach(function(sms) {
+      socket.emit('existing SMS', sms);
+    });
+  });
+});
+
+server.listen(app.get('port'), function() {
   console.log("Express server listening on port " + app.get('port'));
 });
